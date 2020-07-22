02a2bc2e556a23006d65d516af0db4d40f312e38
faquini-gilberto <faquini.gilberto@gmail.com>
2020-07-22 17:55:42 -0300

    Adding DataBase Access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,12 +1,15 @@
 package model.entities;
 
-public class Department {
-			
+import java.io.Serializable;
+
+public class Department implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
-	private String name;
+	private String	name;
 	
 	public Department() {
-		
 	}
 
 	public Department(Integer id, String name) {
@@ -53,6 +56,11 @@ public class Department {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
 	}
 	
 	
